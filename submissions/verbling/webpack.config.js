c668e7add98130cebba1fd414127b0cd4387eaf9
Edmund Li <edmund.xz.lee@gmail.com>
2016-01-03 20:24:33 -0800

    added npm packages

--- submissions/verbling/webpack.config.js
+++ submissions/verbling/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   module: {
       loaders: [
         {
-          test: /.jsx?$/,
+          test: /\.jsx?$/,
           loader: 'babel-loader',
           exclude: /node_modules/,
         },
