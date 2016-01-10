9bf9459a883c1c7846816434eeb97c6ff2791b07
Edmund Li <edmund.xz.lee@gmail.com>
2016-01-09 17:44:26 -0800

    added test for scroll

--- submissions/verbling/test/reducer_spec.js
+++ submissions/verbling/test/reducer_spec.js
@@ -18,4 +18,106 @@ describe('reducer', () => {
       planet: 'Dagobah'
     }));
   });
+
+  it('handles scroll up', () => {
+    const sith1 = Map({
+      id: 5105,
+      name: 'Xendor',
+      homeworld: {
+        id: 58,
+        name: 'Coruscant',
+      },
+      master: null,
+      apprentice: 4629,
+    });
+    const sith2 = Map({
+      id: 4629,
+      name: 'Ajunta Pall',
+      homeworld: {
+        id: 19,
+        name: 'Alderaan',
+      },
+      master: 5105,
+      apprentice: 4601,
+    });
+    const sith3 = Map({
+      id: 4601,
+      name: 'Simus',
+      homeworld: {
+        id: 27,
+        name: 'Korriban',
+      },
+      master: 4629,
+      apprentice: 2950,
+    });
+    const empty1 = Map({
+      id: 6
+    });
+    const empty2 = Map({
+      id: 7
+    });
+    const initialState = fromJS({
+      darkJedis: List.of(sith1, sith2, sith3)
+    });
+    const action = {
+      type: 'SCROLL',
+      dir: 'up'
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      darkJedis: List.of(sith3, empty1, empty2)
+    }));
+  });
+
+  it('handles scroll down', () => {
+    const sith1 = Map({
+      id: 5105,
+      name: 'Xendor',
+      homeworld: {
+        id: 58,
+        name: 'Coruscant',
+      },
+      master: null,
+      apprentice: 4629,
+    });
+    const sith2 = Map({
+      id: 4629,
+      name: 'Ajunta Pall',
+      homeworld: {
+        id: 19,
+        name: 'Alderaan',
+      },
+      master: 5105,
+      apprentice: 4601,
+    });
+    const sith3 = Map({
+      id: 4601,
+      name: 'Simus',
+      homeworld: {
+        id: 27,
+        name: 'Korriban',
+      },
+      master: 4629,
+      apprentice: 2950,
+    });
+    const empty1 = Map({
+      id: 8
+    });
+    const empty2 = Map({
+      id: 9
+    });
+    const initialState = fromJS({
+      darkJedis: List.of(sith1, sith2, sith3)
+    });
+    const action = {
+      type: 'SCROLL',
+      dir: 'down'
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      darkJedis: List.of(empty1, empty2, sith1, sith2, sith3)
+    }));
+  });
 })
